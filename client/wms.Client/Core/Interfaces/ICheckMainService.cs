02fb036b899a657d2dfe20587a1c55c492f60a83
JankeGao <1101336772@qq.com>
2023-09-06 09:35:27 +0800

    0906 update

---
+++ client/wms.Client/Core/Interfaces/ICheckMainService.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+using Bussiness.Dtos;
+using Bussiness.Entitys;
+using HP.Data.Orm;
+using HP.Utility.Data;
+
+namespace wms.Client.Core.Interfaces
+{
+    public interface ICheckMainService
+    {
+        Task<DataResult> PostDoHandCheckClient(CheckDto model);
+
+        Task<DataResult> PostPDACheckComplete(CheckDto model);
+
+        Task<DataResult> PostDoSubmit(CheckMain model);
+    }
+}
---
+++ client/wms.Client/Service/Service/CheckMainService.cs
@@ -0,0 +1,34 @@
+﻿using System.Threading.Tasks;
+using Bussiness.Dtos;
+using Bussiness.Entitys;
+using HP.Utility.Data;
+using wms.Client.Core.Interfaces;
+using wms.Client.Model.RequestModel;
+
+namespace wms.Client.Service.Service
+{
+    public class CheckMainService : ICheckMainService
+    {
+        public async Task<DataResult> PostDoHandCheckClient(CheckDto model)
+        {
+            BaseServiceRequest<DataResult> baseService = new BaseServiceRequest<DataResult>();
+            var r = await baseService.GetRequest<DataResult>(new PostDoHandCheckClient(), model, RestSharp.Method.POST);
+            return r;
+        }
+
+        public async Task<DataResult> PostPDACheckComplete(CheckDto model)
+        {
+            BaseServiceRequest<DataResult> baseService = new BaseServiceRequest<DataResult>();
+            var r = await baseService.GetRequest<DataResult>(new PostPDACheckComplete(), model, RestSharp.Method.POST);
+            return r;
+        }
+
+        public async Task<DataResult> PostDoSubmit(CheckMain model)
+        {
+            BaseServiceRequest<DataResult> baseService = new BaseServiceRequest<DataResult>();
+            var r = await baseService.GetRequest<DataResult>(new PostDoSubmit(), model, RestSharp.Method.POST);
+            return r;
+        }
+
+    }
+}
